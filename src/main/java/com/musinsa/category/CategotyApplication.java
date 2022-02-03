cd84e1e0ed8aacd0516e246bdf7d629c9228dc12
jungsoohyeon <cjdwnvlqkek@gmail.com>
2022-02-03 23:47:50 +0900

    PR Test

--- src/main/java/com/musinsa/category/CategotyApplication.java
+++ src/main/java/com/musinsa/category/CategotyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CategotyApplication {
-//
+//PR TEST
 	public static void main(String[] args) {
 		SpringApplication.run(CategotyApplication.class, args);
 	}
