8ad1ef8070ce4130e02df3a5c0ba07dd78e7cc34
jungsoohyeon <tngus3722@koreatech.ac.kr>
2022-02-03 22:33:14 +0900

    git test

--- src/main/java/com/musinsa/category/CategotyApplication.java
+++ src/main/java/com/musinsa/category/CategotyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CategotyApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(CategotyApplication.class, args);
 	}
