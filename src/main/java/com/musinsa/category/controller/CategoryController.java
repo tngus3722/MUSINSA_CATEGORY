b6b9b0d1e1345ff9b6e3459c3e0f6e619eb77612
jungsoohyeon <cjdwnvlqkek@gmail.com>
2022-02-06 23:39:24 +0900

    e

--- src/main/java/com/musinsa/category/controller/CategoryController.java
+++ src/main/java/com/musinsa/category/controller/CategoryController.java
@@ -4,13 +4,18 @@ import com.musinsa.category.annotation.ValidationGroup;
 import com.musinsa.category.domain.category.CategoryRequest;
 import com.musinsa.category.domain.category.CategoryResponse;
 import com.musinsa.category.service.category.CategoryService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,20 +23,19 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-
     @GetMapping("/category")
     public ResponseEntity<List<CategoryResponse>> getCategory() {
-        return new ResponseEntity<List<CategoryResponse>>(categoryService.getCategory(), HttpStatus.OK);
+        return new ResponseEntity<>(categoryService.getCategory(), HttpStatus.OK);
     }
 
     @GetMapping("/category/{category_id}")
-    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable(value = "category_id") Long categoryId) throws Exception {
-        return new ResponseEntity<CategoryResponse>(categoryService.getCategoryById(categoryId), HttpStatus.OK);
+    public ResponseEntity<CategoryResponse> getCategoryById(@PathVariable(value = "category_id") Long categoryId) {
+        return new ResponseEntity<>(categoryService.getCategoryById(categoryId), HttpStatus.OK);
     }
 
     @PostMapping("/category")
     public ResponseEntity<CategoryResponse> postCategory(@RequestBody @Validated(ValidationGroup.Create.class) CategoryRequest categoryRequest) {
-        return new ResponseEntity<CategoryResponse>(categoryService.postCategory(categoryRequest), HttpStatus.CREATED);
+        return new ResponseEntity<>(categoryService.postCategory(categoryRequest), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/category/{category_id}")
@@ -42,7 +46,7 @@ public class CategoryController {
 
     @PutMapping("/category/{category_id}")
     public ResponseEntity<CategoryResponse> updateCategory(@PathVariable(value = "category_id") Long categoryId,
-                                                           @RequestBody @Validated(ValidationGroup.Update.class) CategoryRequest categoryRequest) {
-        return new ResponseEntity<CategoryResponse>(categoryService.updateCategory(categoryId, categoryRequest), HttpStatus.OK);
+            @RequestBody @Validated(ValidationGroup.Update.class) CategoryRequest categoryRequest) {
+        return new ResponseEntity<>(categoryService.updateCategory(categoryId, categoryRequest), HttpStatus.OK);
     }
 }
