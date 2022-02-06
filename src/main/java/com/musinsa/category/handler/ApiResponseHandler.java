3c152e7a9cb308bc42839ca21ec746519458e9a7
jungsoohyeon <cjdwnvlqkek@gmail.com>
2022-02-06 19:27:22 +0900

    add basebackage for swagger

--- src/main/java/com/musinsa/category/handler/ApiResponseHandler.java
+++ src/main/java/com/musinsa/category/handler/ApiResponseHandler.java
@@ -6,10 +6,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-@ControllerAdvice
-public class ApiResponseHandler implements ResponseBodyAdvice {
+@ControllerAdvice(basePackages = "com.musinsa.category.controller.category")
+public class ApiResponseHandler implements ResponseBodyAdvice<Object> {
 
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
